f1c9d6288fe720e18541179ebb519027bbc6260b
Virginijus03 <77634732+Virginijus03@users.noreply.github.com>
2021-07-10 12:53:53 +0300

    gimine all done

--- Gimine.js
+++ Gimine.js
@@ -2,14 +2,46 @@ class Gimine {
     constructor() {
         this.list = [];
     }
-    gime(name, year) {
-
-        this.list.push([name, year]);
+    gime(item) {
+        // if (!this.isValidItem(item)) {
+        //     return false;
+        // }
+        item.married = false;
+        this.list.push(item);
     }
     giminesMedis() {
         for (let i = 0; i < this.list.length; i++) {
-            console.log((i + 1) + '. ' + this.list[i] + 'metais ir nera vedes');
+            const zmogus = this.list[i];
+
+            let vestuvinisStatusas = '';
+
+            if (zmogus.married === true) {
+                vestuvinisStatusas = 'yra'
+            }
+            else {
+                vestuvinisStatusas = 'nera';
+            }
+
+            console.log(`${i + 1}. ${zmogus.name} gime ${zmogus.year} ir ${vestuvinisStatusas} vedes.`);
         }
+    }
+    zmogausInformacija(i) {
+        console.log(this.list[i]);
+    }
+    vestuves(index) {
+        this.list[index].married = true;
+    }
+    mire(index) {
+        const updatedList = [];
+        for (let i = 0; i < this.list.length; i++) {
+            if (i !== index) {
+                updatedList.push(this.list[i]);
+            }
+        }
+        this.list = updatedList;
+    }
+    skyrybos(index) {
+        this.list[index].married = false;
     }
 }
 
